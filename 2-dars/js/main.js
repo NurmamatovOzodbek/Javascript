2ef90d246046e94332672d4fa6d5ed6651f7f186
Nurmamatov Ozodbek <nurmamatovozodbek971@gmail.com>
2023-05-16 13:27:08 +0500

    feat: second lesson

---
+++ 2-dars/js/main.js
@@ -0,0 +1,15 @@
+let num =+prompt("Birinchi son?")
+let num2 =prompt("Amal")
+let num3 =+prompt("Ikkinchi son?")
+ if(num2=="+"){
+    alert(num+num3)
+ }
+ else if(num2=="-"){
+     alert(num-num3)
+ }
+  else if(num2=="*"){
+    alert(num*num3)
+ }
+ else if(num2=="/"){
+     alert(num/num3)
+ }
