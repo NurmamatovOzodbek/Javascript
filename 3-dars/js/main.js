4183e6c80cec2a3fea1c95818c10a6231a8c1b39
Nurmamatov Ozodbek <nurmamatovozodbek971@gmail.com>
2023-05-24 09:50:37 +0500

    feat: three lesson

--- 3-dars/js/main.js
+++ 3-dars/js/main.js
@@ -1,5 +1,29 @@
-let a = 15
-let b = a+
-console.log((a))
-console.log((b))
-console.log(typeof(b))
+// let color = prompt("Rangni kiriting")
+
+
+// if(color=="yellow"){
+//   console.log("Green")
+// }
+// else if(color=="green"){
+//   console.log("Red")
+// }
+// else if(color=="red"){
+//   console.log("Yellow")
+// }
+// else{
+//   console.log("Svet yuq")
+// }
+
+let num1 = +prompt("Birinchi sonni kiriting")
+let num2 = +prompt("Ikkinchi sonni kiriting")
+
+
+if(num1>num2){
+  console.log("Birinchi son ikkinchi sondan katta")
+}
+else if(num1<num2){
+  console.log("Birinchi son ikkinchi sondan kichik")
+}
+else if(num1===num2){
+  console.log("Bu ikki son teng")
+}
